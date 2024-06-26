00bbfb84c3b045e30a5bf1af21d9e4088a1d595b
Amr Ojjeh <amrojjeh@outlook.com>
2024-06-26 03:10:38 -0500

    Fix bug where summary -> question doesn't update url

--- ui/page/base.go
+++ ui/page/base.go
@@ -61,7 +61,7 @@ func Sidebar(oob bool, qs []SidebarQuestion, summaryURL string) g.Node {
 					g.If(s.Status == model.PendingQuestionStatus,
 						Img(Class("w-5"), Src("/static/icons/circle-question-solid.svg")),
 					),
-					Button(Class("underline truncate"), htmx.Get(s.URL),
+					Button(Class("underline truncate"), htmx.Get(s.URL), g.If(!s.Target, htmx.PushURL("true")),
 						g.Text(s.Prompt),
 					),
 				)
